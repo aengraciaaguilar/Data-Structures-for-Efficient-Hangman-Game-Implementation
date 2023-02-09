f8afa9481cacfd45929f74dc3f2da7196a82e979
aengraciaaguilar <aengraciaaguilar@gmail.com>
2023-02-09 16:11:33 +0800

    def get_words_from_file

--- hangman.py
+++ hangman.py
@@ -3,3 +3,10 @@
 # Course and Year: BSCOE 2-2
 
 import random
+
+def get_words_from_file(file_path):
+    with open(file_path, "r") as file:
+        words = file.readlines()
+    words = [word.strip() for word in words]
+    return words
+
