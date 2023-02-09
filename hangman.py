3cdd11454126dd8bcdfa4bd38bbccfe3370b76c9
aengraciaaguilar <aengraciaaguilar@gmail.com>
2023-02-09 16:12:41 +0800

    def merge

--- hangman.py
+++ hangman.py
@@ -14,3 +14,16 @@ def get_word():
     words = get_words_from_file("words.txt")
     return random.choice(words)
 
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
